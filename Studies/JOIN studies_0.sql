fb3521d78ffb85e4e1b2e098ed338965cd7f8ed2
Stevan Lopes <neo2geon@gmail.com>
2024-02-15 23:49:48 -0300

    refactor: removido do script a criação da base de dados

--- Studies/JOIN studies_0.sql
+++ Studies/JOIN studies_0.sql
@@ -1,29 +1,3 @@
-CREATE TABLE estudosaleatorios.employees (
-    employee_id INT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    department_id INT
-);
-
-CREATE TABLE estudosaleatorios.departments (
-    department_id INT PRIMARY KEY,
-    department_name VARCHAR(50)
-);
-
-INSERT INTO estudosaleatorios.departments (department_id, department_name)
-VALUES (1, 'Marketing'), (2, 'Sales'), (3, 'HR'), (6, 'Engineering');
-
-INSERT INTO estudosaleatorios.employees (employee_id, first_name, last_name, department_id)
-VALUES 
-    (1, 'John', 'Doe', 1),
-    (2, 'Jane', 'Smith', 2),
-    (3, 'Mary', 'Johnson', 3),
-    (4, 'James', 'Brown', 4),
-    (5, 'Emily', 'Davis', 1),
-    (6, 'Michael', 'Miller', 2),
-    (7, 'Sarah', 'Wilson', 3),
-    (8, 'David', 'Moore', 4);
-    
 -- quais os departamentos de cada funcionário? Traga apenas os funcionários com departamento cadastrado
 select
 	employee_id
